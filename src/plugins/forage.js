9f80bbc30aef3ee1803f40c30d03b845f7d82701
Bakir A <bakir.aitkul@gmail.com>
2020-01-23 11:43:33 +0600

    create forage store plugin

---
+++ src/plugins/forage.js
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+
+class Forage {
+
+  constructor() {
+    this.store = window.localStorage
+  }
+
+  get(key) {
+    if (!key) {
+      throw new Error('Key param is required')
+    }
+
+    const item = this.store.getItem(key)
+
+    try {
+      return JSON.parse(item)
+    } catch (error) {
+      return item
+    }
+  }
+
+  set(key, payload) {
+    if (!key || !payload) {
+      throw new Error(`key: ${key} OR payload ${payload} is missing`)
+    }
+
+    let item
+
+    if (typeof payload === 'object' && payload !== null) {
+      item = JSON.stringify(payload)
+    } else {
+      item = payload
+    }
+
+    this.store.setItem(key, item)
+  }
+
+  delete(key) {
+    if (!key) {
+      throw new Error('Key param is required')
+    }
+
+    this.store.removeItem(key)
+  }
+}
+
+Vue.prototype.$forage = new Forage()
