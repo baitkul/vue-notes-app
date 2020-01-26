833dfb0ae8e9d8771522bce7c37b3ef56c89cd96
Bakir A <bakir.aitkul@gmail.com>
2020-01-26 16:16:06 +0600

    ref: add footer

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,23 @@
         </div>
       </div>
     </Window>
+
+    <footer class="absolute bottom-0 left-0 w-full p-2 text-sm text-center">
+      Made with&nbsp;
+      <a
+        href="https://vuejs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="font-medium underline"
+      >Vue.js</a>
+      &nbsp;&amp;&nbsp;
+      <a
+        href="https://tailwindcss.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="font-medium underline"
+      >Tailwindcss</a>
+    </footer>
   </div>
 </template>
 
